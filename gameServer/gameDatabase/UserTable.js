2f9216a2e9d644f49272521fd19208dccf75c10f
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-24 16:07:15 -0500

    added datatable models

---
+++ gameServer/gameDatabase/UserTable.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+const gameDatabase = require("..");
+
+const User = gameDatabase.define('userTable', {
+    userID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userName: {
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    }
+})
+
+
+modules.export = User
