74adf23552e93e5063282d823be0588606ec2501
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-25 00:20:00 -0500

    Added dotenv depend

--- server/database/databaseSeeder.js
+++ server/database/databaseSeeder.js
@@ -1,48 +1,48 @@
 const axios = require("axios")
-const {ActionGames,IndieGames,AdventureGames,ShooterGames,RPGGames} = require("./models/gameTables");
+const { ActionGames, IndieGames, AdventureGames, ShooterGames, RPGGames } = require("./models/gameTables");
 
-const seedDatabase = async ()=>{
+const seedDatabase = async () => {
 
-const actionList = (await axios.get("https://api.rawg.io/api/games?genres=action&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
-const indieList = (await axios.get("https://api.rawg.io/api/games?genres=indie&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
-const adventureList = (await axios.get("https://api.rawg.io/api/games?genres=adventure&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
-const shooterList = (await axios.get("https://api.rawg.io/api/games?genres=shooter&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
-const rpgList = (await axios.get("https://api.rawg.io/api/games?genres=5&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results 
+    const actionList = (await axios.get("https://api.rawg.io/api/games?genres=action&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
+    const indieList = (await axios.get("https://api.rawg.io/api/games?genres=indie&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
+    const adventureList = (await axios.get("https://api.rawg.io/api/games?genres=adventure&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
+    const shooterList = (await axios.get("https://api.rawg.io/api/games?genres=shooter&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
+    const rpgList = (await axios.get("https://api.rawg.io/api/games?genres=5&page_size=5&key=72b5a91b3a92453086806917704da93e")).data.results
     console.log(`------------------\nFinished retreiving initial game data`)
 
-for(let element of actionList){
+    for (let element of actionList) {
         let temp = (await axios.get(`https://api.rawg.io/api/games/${element.id}?key=72b5a91b3a92453086806917704da93e`)).data
-        let arrImages= (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
-        arrImages = arrImages.map((element)=>element.image)
-        await ActionGames.findOrCreate({where:{gameName:temp.name,tally:0,description:temp.description_raw,backgroundImg:temp.background_image,images:arrImages}})
+        let arrImages = (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
+        arrImages = arrImages.map((element) => element.image)
+        await ActionGames.findOrCreate({ where: { gameName: temp.name, tally: 0, description: temp.description_raw, backgroundImg: temp.background_image, images: arrImages } })
     }
     console.log(`------------------\nFinished loading /action!`)
-for(let element of indieList){
+    for (let element of indieList) {
         let temp = (await axios.get(`https://api.rawg.io/api/games/${element.id}?key=72b5a91b3a92453086806917704da93e`)).data
-        let arrImages= (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
-        arrImages = arrImages.map((element)=>element.image)
-        await IndieGames.findOrCreate({where:{gameName:temp.name,tally:0,description:temp.description_raw,backgroundImg:temp.background_image,images:arrImages}})
+        let arrImages = (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
+        arrImages = arrImages.map((element) => element.image)
+        await IndieGames.findOrCreate({ where: { gameName: temp.name, tally: 0, description: temp.description_raw, backgroundImg: temp.background_image, images: arrImages } })
     }
     console.log(`------------------\nFinished loading /indie!`)
-for(let element of adventureList){
+    for (let element of adventureList) {
         let temp = (await axios.get(`https://api.rawg.io/api/games/${element.id}?key=72b5a91b3a92453086806917704da93e`)).data
-        let arrImages= (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
-        arrImages = arrImages.map((element)=>element.image)
-        await AdventureGames.findOrCreate({where:{gameName:temp.name,tally:0,description:temp.description_raw,backgroundImg:temp.background_image,images:arrImages}})
+        let arrImages = (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
+        arrImages = arrImages.map((element) => element.image)
+        await AdventureGames.findOrCreate({ where: { gameName: temp.name, tally: 0, description: temp.description_raw, backgroundImg: temp.background_image, images: arrImages } })
     }
     console.log(`------------------\nFinished loading /adventure!`)
-for(let element of shooterList){
+    for (let element of shooterList) {
         let temp = (await axios.get(`https://api.rawg.io/api/games/${element.id}?key=72b5a91b3a92453086806917704da93e`)).data
-        let arrImages= (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
-        arrImages = arrImages.map((element)=>element.image)
-        await ShooterGames.findOrCreate({where:{gameName:temp.name,tally:0,description:temp.description_raw,backgroundImg:temp.background_image,images:arrImages}})
+        let arrImages = (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
+        arrImages = arrImages.map((element) => element.image)
+        await ShooterGames.findOrCreate({ where: { gameName: temp.name, tally: 0, description: temp.description_raw, backgroundImg: temp.background_image, images: arrImages } })
     }
     console.log(`------------------\nFinished loading /shooter!`)
-for(let element of rpgList){
+    for (let element of rpgList) {
         let temp = (await axios.get(`https://api.rawg.io/api/games/${element.id}?key=72b5a91b3a92453086806917704da93e`)).data
-        let arrImages= (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
-        arrImages = arrImages.map((element)=>element.image)
-        await RPGGames.findOrCreate({where:{gameName:temp.name,tally:0,description:temp.description_raw,backgroundImg:temp.background_image,images:arrImages}})
+        let arrImages = (await axios.get(`https://api.rawg.io/api/games/${element.id}/screenshots?key=72b5a91b3a92453086806917704da93e`)).data.results
+        arrImages = arrImages.map((element) => element.image)
+        await RPGGames.findOrCreate({ where: { gameName: temp.name, tally: 0, description: temp.description_raw, backgroundImg: temp.background_image, images: arrImages } })
     }
     console.log(`------------------\nFinished loading /rpg!\n------------------\nDone loading!`)
 }
