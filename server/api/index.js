3cf45c4e4f233061a1597ffce24a7d393bfada35
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-25 16:36:33 -0500

    added user google login and gitignore file

--- server/api/index.js
+++ server/api/index.js
@@ -1,62 +1,114 @@
-const Router = require('express').Router()
-const {Games,gameDatabase} = require('../database')
-const {Op} = require('sequelize')
+const res = require("express/lib/response");
+const session = require("express-session");
+const passport = require("passport");
+const Router = require("express").Router();
+const { Games, gameDatabase } = require("../database");
+const { Op } = require("sequelize");
+require("./auth");
 
-Router.use('/action', require('./actiongames'))
-Router.use('/indie', require('./indiegames'))
-Router.use('/adventure', require('./adventuregames'))
-Router.use('/shooter', require('./shootergames'))
-Router.use('/rpg', require('./rpggames'))
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
 
-Router.get('/', (req,res)=>{
-    try {res.send("Welcome to the API level!")}
-    catch(error){res.send(error.message)}
-})
 
-Router.get('/all', async (req, res) => {
-    try {
-      const games = await Games.findAll()
-      .then((games) => {
-      res.send(games)
-    })} catch (error) {
-      res.send(error.message)
-    }
+Router.use(session({ secret: "cats" }));
+Router.use(passport.initialize());
+Router.use(passport.session());
+Router.use("/action", require("./actiongames"));
+Router.use("/indie", require("./indiegames"));
+Router.use("/adventure", require("./adventuregames"));
+Router.use("/shooter", require("./shootergames"));
+Router.use("/rpg", require("./rpggames"));
+
+Router.get("/", (req, res) => {
+  res.send('<a href ="/auth/google/"> "Login with Gmail" </a>');
+});
+Router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
   })
-Router.put('/:name')
-Router.get('/fetch25', async (req, res) => {
-    try {
-      var rand = {action:[],indie:[],adventure:[],rpg:[],shooter:[]}
-      const games = await Games.findAll({order : gameDatabase.random()})
-        
-      for(let element of games)
-      {
-        if(rand.action.length < 5 && element.genres.includes("Action")){rand.action.push(element);continue;}
-        if(rand.indie.length < 5 && element.genres.includes("Indie")){rand.indie.push(element);continue;}
-        if(rand.adventure.length < 5 && element.genres.includes("Adventure")){rand.adventure.push(element);continue;}
-        if(rand.rpg.length < 5 && element.genres.includes("RPG")){rand.rpg.push(element);continue;}
-        if(rand.shooter.length < 5 && element.genres.includes("Shooter")){rand.shooter.push(element);continue;}
+);
+
+Router.get("/auth/failure", (req, reas) => {
+  res.send("Please try logging in again");
+});
+
+Router.get("/protected", isLoggedIn, (req, res) => {
+  res.send("Hello");
+});
+
+Router.get("/all", async (req, res) => {
+  try {
+    const games = await Games.findAll().then((games) => {
+      res.send(games);
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+Router.put("/:name");
+Router.get("/fetch25", async (req, res) => {
+  try {
+    var rand = { action: [], indie: [], adventure: [], rpg: [], shooter: [] };
+    const games = await Games.findAll({ order: gameDatabase.random() });
+
+    for (let element of games) {
+      if (rand.action.length < 5 && element.genres.includes("Action")) {
+        rand.action.push(element);
+        continue;
       }
-        res.send(rand)} 
-        catch (error) {
-      res.send(error.message)
-    }
-  })
-  Router.get('/leaderboard', async (req, res) => {
-    try {
-      var lb = {action:[],indie:[],adventure:[],rpg:[],shooter:[]}
-      const games = await Games.findAll({where: {metaRating:{[Op.not]:['null']}},order: [['metaRating','DESC']]})
-      //sorting by metacritic rating
-      for(let element of games)
-      {
-        if(lb.action.length < 5 && element.genres.includes("Action"))lb.action.push(element)
-        if(lb.indie.length < 5 && element.genres.includes("Indie"))lb.indie.push(element)
-        if(lb.adventure.length < 5 && element.genres.includes("Adventure"))lb.adventure.push(element)
-        if(lb.rpg.length < 5 && element.genres.includes("RPG"))lb.rpg.push(element)
-        if(lb.shooter.length < 5 && element.genres.includes("Shooter"))lb.shooter.push(element)
+      if (rand.indie.length < 5 && element.genres.includes("Indie")) {
+        rand.indie.push(element);
+        continue;
       }
-        res.send(lb)} 
-        catch (error) {
-      res.send(error.message)
+      if (rand.adventure.length < 5 && element.genres.includes("Adventure")) {
+        rand.adventure.push(element);
+        continue;
+      }
+      if (rand.rpg.length < 5 && element.genres.includes("RPG")) {
+        rand.rpg.push(element);
+        continue;
+      }
+      if (rand.shooter.length < 5 && element.genres.includes("Shooter")) {
+        rand.shooter.push(element);
+        continue;
+      }
     }
-  })
-module.exports = Router
+    res.send(rand);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+Router.get("/leaderboard", async (req, res) => {
+  try {
+    var lb = { action: [], indie: [], adventure: [], rpg: [], shooter: [] };
+    const games = await Games.findAll({
+      where: { metaRating: { [Op.not]: ["null"] } },
+      order: [["metaRating", "DESC"]],
+    });
+    //sorting by metacritic rating
+    for (let element of games) {
+      if (lb.action.length < 5 && element.genres.includes("Action"))
+        lb.action.push(element);
+      if (lb.indie.length < 5 && element.genres.includes("Indie"))
+        lb.indie.push(element);
+      if (lb.adventure.length < 5 && element.genres.includes("Adventure"))
+        lb.adventure.push(element);
+      if (lb.rpg.length < 5 && element.genres.includes("RPG"))
+        lb.rpg.push(element);
+      if (lb.shooter.length < 5 && element.genres.includes("Shooter"))
+        lb.shooter.push(element);
+    }
+    res.send(lb);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+module.exports = Router;
