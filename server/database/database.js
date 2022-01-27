542ef054505b547245aea1cb8af8ae1cfe7bba1e
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-27 14:18:40 -0500

    changed conneciton string to accept ssl

--- server/database/database.js
+++ server/database/database.js
@@ -1,4 +1,14 @@
 const Sequelize = require('sequelize')
 //console.log("postgres://" + process.env.DB_USERNAME +":"+ process.env.DB_PASSWORD + "@"+ process.env.DB_HOSTNAME +"/capstone");
-const gameDatabase = new Sequelize("postgres://" + process.env.DB_USERNAME +":"+process.env.DB_PASSWORD + "@"+ process.env.DB_HOSTNAME +"/"+ process.env.DB_NAME,{logging : false})
+//const gameDatabase = new Sequelize("postgres://" + process.env.DB_USERNAME +":"+process.env.DB_PASSWORD + "@"+ process.env.DB_HOSTNAME +"/"+ process.env.DB_NAME,{logging : false})
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const gameDatabase = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
+
 module.exports = gameDatabase
